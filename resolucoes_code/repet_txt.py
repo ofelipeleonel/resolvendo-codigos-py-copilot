8230d39b7e01cf583c816cb24d47875286add28e
ofelipeleonel <felipeleonelmachado@hotmail.com>
2026-03-12 20:29:21 -0300

    Feat somar dois números

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,8 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+number_1 = int(input("Digite um número: "))
+number_2= int(input("Digite outro número: "))
+
+soma = number_1 + number_2
+
+print(soma)
