460d5f716da83b814390ff04ffc69a7692890ed8
ofelipeleonel <felipeleonelmachado@hotmail.com>
2026-03-12 20:15:05 -0300

    update operação de soma

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,8 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+number_1 = int(input("Digite o primeiro número: "))
+number_2 = int(input("Digite o segundo número: "))
+
+soma = number_1 + number_2
+
+print(soma)
