6da6a4e50595bf77d5e0062a2e391476deb74b3e
ofelipeleonel <felipeleonelmachado@hotmail.com>
2026-03-12 20:10:45 -0300

    Update concatenação de dados

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,7 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+nome = input("Digite seu nome: ")
+ocupacao = input("Digite sua ocupação: ")
+
+frase_com_dados_concatenados = "Olá, " + nome + "! Tudo bem? Veriquei que é " + ocupacao + ", que interessante."
+print(frase_com_dados_concatenados)
